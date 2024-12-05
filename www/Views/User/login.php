8f4f93cf4f7d95d5d0782d5ee007c2cae4103ba7
WilsonSan5 <wilson.san57@gmail.com>
2024-12-05 20:15:49 +0100

    new version login

--- www/Views/User/login.php
+++ www/Views/User/login.php
@@ -1,10 +1,31 @@
 <?php
 use App\Core\Messages;
+
 $messages = Messages::getMessages();
+if (!empty($messages)) {
+    $messageType = '';
+    $messageContent = '';
+    foreach ($messages as $message) {
+        $messageType = $message['type'];
+        $messageContent = $message['message'];
+    }
+}
 ?>
 
-<form method="POST">
-    <input type="email" placeholder="email" name="email">
-    <input placeholder="password" name="password" type="password">
-    <input type="submit">
+<form class="form" action="/login" method="post">
+    <div class="title">Connexion</div>
+    <span class="message message-<?= $messageType ?>">
+        <?= $messageContent ?? '' ?>
+    </span>
+    <div class="input-container ic2">
+        <input id="email" class="input" name="email" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="email" class="placeholder">Email</>
+    </div>
+    <div class="input-container ic2">
+        <input id="lastname" class="input" name="password" type="password" placeholder=" " />
+        <div class="cut"></div>
+        <label for="lastname" class="placeholder">Mot de passe</label>
+    </div>
+    <button type="text" class="submit">Valider</button>
 </form>
